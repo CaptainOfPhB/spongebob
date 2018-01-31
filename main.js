bfd0fe9de2a9679487e3350b8838e0b2b552fe75
戴江涛 <captaininphw@gmail.com>
2018-02-01 01:15:27 +0800

    添加实时编码预览功能

--- main.js
+++ main.js
@@ -15,3 +15,24 @@
         }
     })
 }();
+!function () {
+    let container = document.querySelector('#code');
+    let paper = document.querySelector('#paper');
+    let style = document.querySelector('#style');
+    let n = 1;
+    let timer;
+    timer = setTimeout(function WriteCode() {
+        paper.innerHTML = code.substring(0, n);
+        style.innerHTML = code.substring(0, n);
+        paper.scrollTop = paper.scrollHeight;
+        n += 1;
+        if (n < code.length) {
+            timer = setTimeout(WriteCode, 10);
+        } else {
+            setTimeout(container.classList.add('hide'),3000);
+            if (confirm('要不要去我的 GitHub 看一看呀？')) {
+                window.location.href = 'https://github.com/CaptainInPHW/spongebob';
+            }
+        }
+    }, 10)
+}();
